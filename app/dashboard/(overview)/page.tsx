da3cf1b622cafbf714fe9261f008a4a62c7f22d8
nndang <nndang.sc@gmail.com>
2023-10-30 17:23:52 +0700

    ep 11

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,36 @@
+import { Suspense } from 'react';
+
+import Cards from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import {
+    RevenueChartSkeleton,
+    LatestInvoicesSkeleton,
+    CardsSkeleton,
+} from '@/app/ui/skeletons';
+
+import { inter } from '@/app/ui/fonts';
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${inter.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard + {Date.now()}
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton />}>
+                    <Cards />
+                </Suspense>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                {/*  */}
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
+    );
+}
