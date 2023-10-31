39803777810a8601e426d62959d767328636e44a
nndang <nndang.sc@gmail.com>
2023-10-31 21:51:55 +0700

    ep 15

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -15,19 +15,16 @@ export default function EditInvoiceForm({
     invoice,
     customers,
 }: {
-    invoice: InvoiceForm | undefined;
+    invoice: InvoiceForm;
     customers: CustomerField[];
 }) {
-    const updateInvoiceWithId = updateInvoice.bind(
-        null,
-        invoice === undefined ? '' : invoice.id
-    );
+    const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
     return (
         <form action={updateInvoiceWithId}>
             <div className="rounded-md bg-gray-50 p-4 md:p-6">
                 {/* Invoice ID */}
-                <input type="hidden" name="id" value={invoice?.id} />
+                <input type="hidden" name="id" value={invoice.id} />
                 {/* Customer Name */}
                 <div className="mb-4">
                     <label
@@ -41,7 +38,7 @@ export default function EditInvoiceForm({
                             id="customer"
                             name="customerId"
                             className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                            defaultValue={invoice?.customer_id}
+                            defaultValue={invoice.customer_id}
                         >
                             <option value="" disabled>
                                 Select a customer
@@ -70,7 +67,7 @@ export default function EditInvoiceForm({
                                 id="amount"
                                 name="amount"
                                 type="number"
-                                defaultValue={invoice?.amount}
+                                defaultValue={invoice.amount}
                                 placeholder="Enter USD amount"
                                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                             />
